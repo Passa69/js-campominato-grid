37e7827530679be05b7d9fcf330026bbc7a26b69
Tommaso Passarotto <passarotto13@gmail.com>
2021-11-02 16:19:04 +0100

    aggiunta scelta dell'utente

--- js/script.js
+++ js/script.js
@@ -3,15 +3,20 @@
     // 2.1 creo un ciclo per ogni scelta => for
 // 3. l'utente deve essere in grado di selezionare le celle per rendere selezionate => click + classList
 
+// richiesta all'utente
+const userChoice = parseInt(prompt("Scegli il livello di difficoltà tra: 1, 2 e 3"));
+console.log(userChoice);
 
 // costante del contenitore
 const squareContainer = document.getElementById("container");
 
-// ciclo base per la ripetizione degli square
-for (let i = 1; i <= 100; i++) {
-    let square = document.createElement("div");
-    square.classList.add("square");
-    square.append(i);
 
-    squareContainer.append(square);
-}
+
+// // ciclo base per la ripetizione degli square
+// for (let i = 1; i <= 100; i++) {
+//     let square = document.createElement("div");
+//     square.classList.add("square");
+//     square.append(i);
+
+//     squareContainer.append(square);
+// }
