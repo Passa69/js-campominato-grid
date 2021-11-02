1809cb3b59e75e40554532eeb46c82a37141e7e9
Tommaso Passarotto <passarotto13@gmail.com>
2021-11-02 16:14:23 +0100

    aggiunta dello pseudocodice js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+// 1. chiedo all'utenete che livello di difficoltà desidera => prompt
+// 2. a seconda della scelta modulo il layout => if
+    // 2.1 creo un ciclo per ogni scelta => for
+// 3. l'utente deve essere in grado di selezionare le celle per rendere selezionate => click + classList
+
+
 // costante del contenitore
 const squareContainer = document.getElementById("container");
 
